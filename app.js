7a67bde36c01115d559912abc0b812cf74060be0
Louis Legrain <contact@louislegrain.online>
2021-01-24 16:24:04 +0100

    Use IntersectionObserver

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 const sections = document.querySelectorAll('section');
-const navbarLinks = document.querySelectorAll('nav a[href^="#"]');
-let navbarLinksLi = [];
-for (let navLink of navbarLinks) {
-    navbarLinksLi.push(navLink.parentNode);
-}
-let array = [];
-refreshArray();
 let mobile;
 refreshMobileVar();
 let headerHeight;
 refreshHeaderHeight();
-let lastVisibleSection;
-
 document.querySelector('[href="#home"]').parentNode.id = 'active-page';
 
 window.addEventListener('load', function () {
@@ -31,13 +22,6 @@ function refreshMobileVar() {
     }
 }
 
-function refreshArray() {
-    array = [];
-    for (let section of sections) {
-        array.push(section.offsetTop - window.innerHeight / 2);
-    }
-}
-
 function refreshHeaderHeight() {
     if (mobile) {
         headerHeight = 70;
@@ -46,32 +30,38 @@ function refreshHeaderHeight() {
     }
 }
 
-function checkVisibleSection() {
-    let returnResult;
-    for (let i = 0; i < array.length; i++) {
-        if (window.scrollY > array[i]) {
-            returnResult = i;
-        } else {
-            return returnResult;
-        }
-    }
-    return returnResult;
-}
-
 window.addEventListener('resize', function () {
-    refreshArray();
     refreshMobileVar();
     refreshHeaderHeight();
 });
 
-window.addEventListener('scroll', function () {
-    let checkVisibleSectionId = checkVisibleSection();
-    if (lastVisibleSection !== checkVisibleSectionId) {
-        document.getElementById('active-page').removeAttribute('id');
-        navbarLinksLi[checkVisibleSectionId].id = 'active-page';
-        lastVisibleSection = checkVisibleSectionId;
-    }
-});
+
+
+// Start IntersectionObserver
+const options = {
+    root: null,
+    rootMargin: '-50% 0px -50% 0px'
+};
+
+const handleIntersect = function (entries) {
+    entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+            document.getElementById('active-page').removeAttribute('id');
+            document.querySelector('nav a[href="#' + entry.target.id + '"]').parentNode.id = 'active-page';
+        }
+    });
+}
+  
+const observer = new IntersectionObserver(handleIntersect, options);
+
+if ('IntersectionObserver' in window) {
+    sections.forEach(function (object) {
+        observer.observe(object);
+    });
+}
+// End IntersectionObserver
+
+
 
 for (let hash of document.querySelectorAll('a[href^="#"]')) {
     hash.addEventListener('click', function (e) {
@@ -84,3 +74,46 @@ for (let hash of document.querySelectorAll('a[href^="#"]')) {
         });
     });
 }
+
+
+// Start other solution
+
+// const sections = document.querySelectorAll('section');
+// const navbarLinks = document.querySelectorAll('nav a[href^="#"]');
+// let navbarLinksLi = [];
+// for (let navLink of navbarLinks) {
+//     navbarLinksLi.push(navLink.parentNode);
+// }
+// let array = [];
+// refreshArray();
+// let lastVisibleSection;
+
+// function refreshArray() {
+//     array = [];
+//     for (let section of sections) {
+//         array.push(section.offsetTop - window.innerHeight / 2);
+//     }
+// }
+
+// function checkVisibleSection() {
+//     let returnResult;
+//     for (let i = 0; i < array.length; i++) {
+//         if (window.scrollY > array[i]) {
+//             returnResult = i;
+//         } else {
+//             return returnResult;
+//         }
+//     }
+//     return returnResult;
+// }
+
+// window.addEventListener('scroll', function () {
+//     let checkVisibleSectionId = checkVisibleSection();
+//     if (lastVisibleSection !== checkVisibleSectionId) {
+//         document.getElementById('active-page').removeAttribute('id');
+//         navbarLinksLi[checkVisibleSectionId].id = 'active-page';
+//         lastVisibleSection = checkVisibleSectionId;
+//     }
+// });
+
+// End other solution
